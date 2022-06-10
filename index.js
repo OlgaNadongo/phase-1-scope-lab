119f895f7322cfa7003c35c726ee30294cb42e5a
Olga Nadongo <winnienadongo@gmail.com>
2022-06-10 06:30:59 +0300

    commit tree 969166b9d19f5ed2b6fa5953da3ed79f27ebf6d0

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var customerName= "bob"
+
+function upperCaseCustomerName(){
+    customerName= customerName.toUpperCase();
+}
+
+
+function setBestCustomer(){
+     bestCustomer="not bob";
+    
+
+}
+
+function overwriteBestCustomer(){
+    bestCustomer= 'maybe bob'
+}
+
+//function overWriteBestCustomer(){
+   // bestCustomer="maybe bob";
+    
+//}
+
+const leastFavoriteCustomer="Zia";
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer="Zoey";
+}
